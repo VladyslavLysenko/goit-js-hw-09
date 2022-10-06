3e439c4d36c146648b19a2153da42f931b7739fc
VladyslavLysenko <Dodog12362@gmail.com>
2022-10-06 12:21:06 +0300

    well done task 1
    well done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
+console.log(startBtn);
+console.log(stopBtn);
+let interval = null;
+
+startBtn.addEventListener("click", onClickStart)
+
+function onClickStart(e) {
+  body.style.backgroundColor = `${getRandomHexColor()}`
+  
+  interval = setInterval(
+    () => {
+      body.style.backgroundColor = `${getRandomHexColor()}`
+    },
+    1000);
+  startBtn.setAttribute("disabled", true)
+  stopBtn.removeAttribute("disabled")
+
+}
+
+stopBtn.addEventListener("click", onClickStop)
+function onClickStop(e) {
+  clearInterval(interval)
+  startBtn.removeAttribute("disabled")
+  stopBtn.setAttribute("disabled", true)
+  
+}
 
